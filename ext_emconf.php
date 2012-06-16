189f3a300c3c31a192c4039d547ec4d4b0c28da7
greinerd <greinerd@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-16 11:49:40 +0000

    new ext_emconf after uploading to ter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3_less/trunk@63506 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "t3_less".
 #
-# Auto generated 27-05-2012 09:50
+# Auto generated 16-06-2012 13:49
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'An easy to use extbase extension for using LESScss in TYPO3. You can choose between leafo.net LESS-PHP-compiler or Javascript-based less.js-compiler. It is also possible to include compiled files and delete unused/old compiled files automaticaly.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.4.0',
+	'version' => '0.5.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"50e4";s:14:"ext_tables.php";s:4:"1ea5";s:37:"Classes/Controller/LessController.php";s:4:"6bba";s:38:"Configuration/TypoScript/constants.txt";s:4:"aa5e";s:34:"Configuration/TypoScript/setup.txt";s:4:"49bc";s:40:"Resources/Private/Language/locallang.xml";s:4:"4ef5";s:35:"Resources/Private/Lib/lessc.inc.php";s:4:"db5a";s:37:"Resources/Public/Js/less-1.3.0.min.js";s:4:"ca73";s:14:"doc/manual.sxw";s:4:"1a22";}',
+	'_md5_values_when_last_written' => 'a:10:{s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"50e4";s:14:"ext_tables.php";s:4:"1ea5";s:37:"Classes/Controller/LessController.php";s:4:"a23b";s:38:"Configuration/TypoScript/constants.txt";s:4:"aa5e";s:34:"Configuration/TypoScript/setup.txt";s:4:"065b";s:40:"Resources/Private/Language/locallang.xml";s:4:"4ef5";s:35:"Resources/Private/Lib/lessc.inc.php";s:4:"db5a";s:37:"Resources/Public/Js/less-1.3.0.min.js";s:4:"ca73";s:14:"doc/manual.sxw";s:4:"cff5";}',
+	'suggests' => array(
+	),
 );
 
 ?>
